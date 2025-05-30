1adc468fdcebd0aa9657abb87a2354be635c9f70
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-05-30 19:46:37 +0530

    first commit

---
+++ src/main/java/com/smartEdu/SmartEduBackend/SmartEduBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartEdu.SmartEduBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEduBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEduBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartEdu/SmartEduBackend/SmartEduBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartEdu.SmartEduBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEduBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
