20d06058fdcead3a8f64df1b6681f098a125de0c
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-07-01 20:24:34 +0530

    The Attendance entity and AttendanceStatus ENUM  were created

---
+++ src/main/java/com/smartEdu/SmartEduBackend/entity/Attendance.java
@@ -0,0 +1,29 @@
+package com.smartEdu.SmartEduBackend.entity;
+
+import com.smartEdu.SmartEduBackend.enums.AttendanceStatus;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "attendances")
+public class Attendance {
+
+    @Id
+    private String id;
+
+    private String studentId;
+    private String classId;
+    private String grade;
+
+    private LocalDate date;
+
+    private AttendanceStatus status;
+
+    private String remarks;
+}
